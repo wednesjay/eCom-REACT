5e44f57fe2120b69c21e8035a8215524acd32fb6
Jay Garnes <jgarnes73@gmail.com>
2021-02-05 05:17:43 -0500

    nav fix

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -7,9 +7,9 @@ function Navbar () {
     return (
         <section className = 'navbar'>
             <a href="/" className="navbar-item">Home</a>
-            <a href="/about" className="navbar-item">Pizzas</a>
-            <a href="/portfolio" className="navbar-item">Our Mission</a>
-            <a href="/shop" className="navbar-item">Contact Us</a>
+            <a href="/" className="navbar-item">Pizzas</a>
+            <a href="/" className="navbar-item">Our Mission</a>
+            <a href="/" className="navbar-item">Contact Us</a>
         </section>
     )
 }
